046e7c847cec3d8a6c11db1995444dae07859da3
Nux-xader <sumgmng@gmail.com>
2021-11-02 07:40:51 +0700

    update

--- matrix.cpp
+++ matrix.cpp
@@ -5,28 +5,30 @@ using namespace std;
 int main(){
 	int matrix_a[4];
 	int matrix_b[4];
-	string index[] = {"[0] [0]", "[0] [1]", "[1] [0]", "[1] [1]"};
-	cout << "Namaku adalah : Your name\nUmurku : old\n";
+	string index[] = {"baris 1, kolom 1", "baris 1, kolom 2", "baris 2, kolom 1", "baris 2, kolom 2"};
 	cout << "==============================\nMenampilkan perhitungan matrix\n\n";
 
 	// Get value matrix A
 	cout << "Matrix A :\n";
 	for (int i=0; i<4; i++){
-		cout <<"A "<< index[i]<< " = ";
+		cout <<index[i]<< " : ";
 		cin >> matrix_a[i];
 	}
 
 	// Get value matrix B
 	cout << "\nMatrix B :\n";
 	for (int i=0; i<4; i++){
-		cout <<"B "<< index[i]<< " = ";
+		cout << index[i]<< " : ";
 		cin >> matrix_b[i];
 	}
 
 	// View result
 	cout << "\nHasil penjumlahan matrix :\n";
 	for (int i=0; i<4; i++){
-		cout <<"C "<< index[i]<< " = "<< matrix_a[i]+matrix_b[i]<< "\n";
+		cout << matrix_a[i]+matrix_b[i]<< " ";
+		if ((i+1)%2 == 0) {
+			cout << "\n";
+		}
 	}
 
 	return 0;
